413b2d4055fdd385ceec39634e8c962a081b1961
brookehorizon <danieltashman17@gmail.com>
2022-04-21 08:40:40 -0600

    Updated

--- app/__main__.py
+++ app/__main__.py
@@ -1,3 +1,13 @@
+
+def main():
+    i = input('[C]lient app\n[S]erver app\n:')
+    if i in ['c', 'C']:
+        os.system('python3 app/client_App.py')
+    elif i in ['s', 'S']:
+        os.system('python3 app/server_App.py')
+
+
 if __name__ == '__main__':
     import os
-    os.system('python3 app/client_App.py')
+    main()
+    

1c33fdda76ce83303b5de08914758a0cabe26705
brookehorizon <86805843+brookehorizon@users.noreply.github.com>
2022-05-17 10:56:34 +0300

    Update __main__.py

--- app/__main__.py
+++ app/__main__.py
@@ -1,13 +1,5 @@
 
-def main():
-    i = input('[C]lient app\n[S]erver app\n:')
-    if i in ['c', 'C']:
-        os.system('python3 app/client_App.py')
-    elif i in ['s', 'S']:
-        os.system('python3 app/server_App.py')
-
-
 if __name__ == '__main__':
     import os
-    main()
+    os.system('python3 app/main.py')
     
